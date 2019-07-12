b35fba4d69b9f92334bd784eac3b94934ba37607
Andrew Hsieh <anew7237@gmail.com>
2019-07-12 23:32:54 +0800

    20190712a
    Signed-off-by: Andrew Hsieh <anew7237@gmail.com>

--- alpine3.9.x-auto/_COPY_/x_script/root.sh
+++ alpine3.9.x-auto/_COPY_/x_script/root.sh
@@ -4,8 +4,10 @@ set -xe
 
 echo "root.sh START"
 
-
+if [ -f /x_script/etc/resolv.conf ] && [ "$(cat /x_script/etc/resolv.conf | grep '###ANEW:')" != "$(cat /etc/resolv.conf | grep '###ANEW:')" ]  ; then
+	cat /x_script/etc/resolv.conf > /etc/resolv.conf
+fi
 
 echo "root.sh End!!"
 
-exec "$@"
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
